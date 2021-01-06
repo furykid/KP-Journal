7473c677ebb54fc880b4e52da6f7f7a99d5099d5
Nick Mauro <furykid777@gmail.com>
2021-01-06 10:45:22 -0500

    Updated data, added test for journal entries list

--- src/components/JournalEntriesList.js
+++ src/components/JournalEntriesList.js
@@ -13,39 +13,30 @@ function JournalEntriesList(props) {
       <div>
         {props.journalEntries.map((entry) => {
           return (
-            <div>
-              <ListGroup key={entry.id}>
-                <div>&nbsp;</div>
-                <ListGroupItem
-                  tag="a"
-                  href={"/user/" + entry.userId + "/journalEntries/" + entry.id}
-                  action
-                >
-                  <ListGroupItemHeading>
-                    {new Date(entry.date).toDateString()}
-                  </ListGroupItemHeading>
-                  <ListGroupItemText>
-                    {entry.tags.map((tag) => {
-                      return (
-                        <>
-                          {" "}
-                          <Button variant="outline-info" size="sm" disabled>
-                            {tag}
-                          </Button>
-                        </>
-                      );
-                    })}
-                  </ListGroupItemText>
-                  <ListGroupItemText>
-                    calories: {entry.calories}
-                  </ListGroupItemText>
-                  <ListGroupItemText>
-                    sleep: {entry.sleep} hours
-                  </ListGroupItemText>
-                  <ListGroupItemText>notes: {entry.notes}</ListGroupItemText>
-                </ListGroupItem>
-              </ListGroup>
-            </div>
+            <ListGroup key={entry.id}>
+              <div>&nbsp;</div>
+              <ListGroupItem
+                tag="a"
+                href={"/user/" + entry.userId + "/journalEntries/" + entry.id}
+                action
+              >
+                <ListGroupItemHeading>
+                  {new Date(entry.date).toDateString()}
+                </ListGroupItemHeading>
+                <ListGroupItemText>
+                  <Button variant="outline-info" size="sm" disabled>
+                    {entry.tag}
+                  </Button>
+                </ListGroupItemText>
+                <ListGroupItemText>
+                  calories: {entry.calories}
+                </ListGroupItemText>
+                <ListGroupItemText>
+                  sleep: {entry.sleep} hours
+                </ListGroupItemText>
+                <ListGroupItemText>notes: {entry.notes}</ListGroupItemText>
+              </ListGroupItem>
+            </ListGroup>
           );
         })}
       </div>
---
+++ src/test/JournalEntriesList.test.js
@@ -0,0 +1,95 @@
+import JournalEntriesList from "../../src/components/JournalEntriesList";
+import { configure, mount } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { BrowserRouter as Router } from "react-router-dom";
+configure({ adapter: new Adapter() });
+
+const _journalEntries = [
+  {
+    id: 1,
+    date: "2020-05-23T18:25:43.511Z",
+    userId: 0,
+    tag: "snatch",
+    weightFormat: "kg",
+    sleep: 8,
+    calories: 1500,
+    notes: "Everything felt great",
+    exercises: [
+      {
+        id: 1,
+        exercise: "snatch recoveries",
+        set: 1,
+        weight: 80,
+        reps: 1,
+        pr: true,
+        notes: "Shoulders were a little sore",
+      },
+    ],
+  },
+  {
+    id: 2,
+    date: "2020-05-23T18:25:43.511Z",
+    userId: 1,
+    tag: "clean",
+    weightFormat: "kg",
+    sleep: 8,
+    calories: 1500,
+    notes: "Everything felt great",
+    exercises: [
+      {
+        id: 1,
+        exercise: "snatch recoveries",
+        set: 1,
+        weight: 40,
+        reps: 5,
+        pr: false,
+        notes: "Shoulders were a little sore",
+      },
+    ],
+  },
+];
+
+describe("Journal Entries render tests", () => {
+  it("renders journal entries list from props", () => {
+    const wrapper = mount(
+      <Router>
+        <JournalEntriesList journalEntries={_journalEntries} />
+      </Router>
+    );
+
+    expect(wrapper.props().children.props.journalEntries).toEqual(
+      _journalEntries
+    );
+  });
+
+  it("renders journal entry specific data to list", () => {
+    const wrapper = mount(
+      <Router>
+        <JournalEntriesList journalEntries={_journalEntries} />
+      </Router>
+    );
+
+    // There should be ONLY 1 table element
+    const list = wrapper.find("ul");
+    expect(list).toHaveLength(_journalEntries.length);
+
+    list.forEach((li, rowIndex) => {
+      const element = li.find("a");
+      expect(element.childAt(0).text()).toContain(
+        new Date(_journalEntries[rowIndex].date).toDateString()
+      );
+      expect(element.childAt(1).text()).toContain(
+        _journalEntries[rowIndex].tag
+      );
+      expect(element.childAt(2).text()).toContain(
+        _journalEntries[rowIndex].calories
+      );
+      expect(element.childAt(3).text()).toContain(
+        _journalEntries[rowIndex].sleep
+      );
+      expect(element.childAt(4).text()).toContain(
+        _journalEntries[rowIndex].notes
+      );
+    });
+  });
+});
