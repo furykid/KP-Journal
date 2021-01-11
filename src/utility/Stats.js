4653fe4d943bcf1a6b9a276c36d7c4543253d672
Nick Mauro <furykid777@gmail.com>
2021-01-11 08:09:08 -0500

    Housekeeping

--- src/utility/Stats.js
+++ src/utility/Stats.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { NavbarBrand } from 'reactstrap';
 
-let total = 0;
-
 function Stats(props) {
   function getTotalWeight() {
-    total = 0;
-    total = props.exercises.reduce((tot, exercise) => {
+    return props.exercises.reduce((tot, exercise) => {
       return tot + exercise.weight * exercise.reps;
     }, 0);
-
-    console.log(total);
-    return total;
   }
 
   return (
