18b8ad3bd1249b259bef283d01a690bc05f845e8
Nick Mauro <furykid777@gmail.com>
2021-01-25 09:58:30 -0500

    Added fields to entry form

--- src/components/JournalEntryForm.js
+++ src/components/JournalEntryForm.js
@@ -4,6 +4,9 @@ import Button from 'react-bootstrap/Button';
 
 function JournalEntryForm(props) {
   const [date, setDate] = useState(new Date().toLocaleDateString());
+  const [calories, setCalories] = useState('');
+  const [sleep, setSleep] = useState('');
+  const [notes, setNotes] = useState('');
 
   function handleSubmit() {}
 
@@ -22,6 +25,24 @@ function JournalEntryForm(props) {
             onChange={(event) => {
               setDate(event.target.value);
             }}
+          ></Form.Control>
+          <Form.Label>Calories</Form.Label>
+          <Form.Control
+            type='string'
+            value={calories}
+            onChange={(event) => setCalories(event.target.value)}
+          ></Form.Control>
+          <Form.Label>Sleep</Form.Label>
+          <Form.Control
+            type='string'
+            value={sleep}
+            onChange={(event) => setSleep(event.target.value)}
+          ></Form.Control>
+          <Form.Label>Notes</Form.Label>
+          <Form.Control
+            as='textarea'
+            value={notes}
+            onChange={(event) => setNotes(event.target.value)}
           ></Form.Control>
         </Form.Group>
         <Button type='submit' className='my-1 float-right'>
