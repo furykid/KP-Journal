33100a323d7d439835e3f7cb21b84057c7b49fd4
Nick Mauro <furykid777@gmail.com>
2021-02-18 18:16:00 -0500

    Build errors... again

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -34,7 +34,7 @@ function HomePage(props) {
       }
     };
     getUserMetadata();
-  }, [getAccessTokenSilently, user.sub]);
+  }, [getAccessTokenSilently, user]);
 
   if (isLoading) {
     return <img src={loadingImg} alt='loading...' />;
